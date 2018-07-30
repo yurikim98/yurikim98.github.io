d2e122f681174f090f396d872192ceca2c889241
yurikim98 <41550921+yurikim98@users.noreply.github.com>
2018-07-31 00:50:39 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,32 +1,8 @@
 "use strict"
-// sidebar
-const toggler = document.getElementById('navbar-toggler');
-const app = document.getElementById('app');
-const navbar = document.getElementById('navbar-nav');
-toggler.addEventListener('click', function() {
-    app.classList.toggle('absolute');
-    app.classList.toggle('opened');
-    navbar.classList.toggle('opened');
-});
-// scrolltop
-$(document).ready(function() {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 0) {
-            $('#scroller').fadeIn();
-        } else {
-            $('#scroller').fadeOut();
-        }
-    });
-    $('#scroller').click(function() {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 500);
-        return false;
-    });
-    $('#scrolldown').click(function() {
-        $('body,html').animate({
-            scrollTop: $('body').height()
-        }, 500);
-        return false;
-    });
-});
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+        .then(reg => console.log('Registered', reg))
+        .catch(err => console.error('error:', err));
+} else {
+    console.log(0);
+}
